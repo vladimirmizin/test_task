9d9440f156bcafa7a62378e8337f9e3a888c5fa4
vladimirmizin <bar@example.com>
2019-11-18 11:03:22 +0200

    remove console.log

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,6 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -44,7 +43,6 @@
             });
         });
         $(document).on('click',".showbutton", function () {
-            console.log('123');
             $(this).siblings("#showblock").toggle("slow");
         });
     });
